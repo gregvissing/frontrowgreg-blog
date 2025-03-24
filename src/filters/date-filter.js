0a66511590a1dfff93bc8863ddef30eacf3ba7b6
Greg Vissing <greg.vissing@gmail.com>
2025-03-24 14:54:58 +0000

    Update date of Post format

--- src/filters/date-filter.js
+++ src/filters/date-filter.js
@@ -9,7 +9,7 @@ module.exports = function dateFilter(value) {
   const dateObject = new Date(value);
 
   const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-  const dayWithSuffix = appendSuffix(dateObject.getDate());
+  const dayWithSuffix = appendSuffix(dateObject.getDate()) + ",";
 
-  return `${dayWithSuffix} ${months[dateObject.getMonth()]} ${dateObject.getFullYear()}`;
+  return `${months[dateObject.getMonth()]} ${dayWithSuffix} ${dateObject.getFullYear()}`;
 };
